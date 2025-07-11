926d46f4976e373fc06d3789f25b6c74eea20e85
Labreo <74888611+Labreo@users.noreply.github.com>
2025-07-11 15:49:56 +0530

    Added a database

---
+++ init_db.py
@@ -0,0 +1,31 @@
+import sqlite3
+#Dont run this again pls.
+# Connect to SQLite database (or create it)
+conn = sqlite3.connect('pickups.db')
+cursor = conn.cursor()
+
+# Create 'users' table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+)
+''')
+
+#Create pickup table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS pickups (
+    user_id INTEGER NOT NULL,
+    address TEXT NOT NULL,
+    waste_type TEXT NOT NULL,
+    status TEXT DEFAULT 'pending', 
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
+''')
+
+
+conn.commit()
+conn.close()
+print("Database and users table created.")
